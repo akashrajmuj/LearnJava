438e4081734fd39ab3365fe0082fd029dc7be0c5
Akash Raj <43521630+akashrajmuj@users.noreply.github.com>
2020-10-03 21:15:29 +0530

    Updated Main class, made it more user friendly.

--- chapter1/MainClass.java
+++ chapter1/MainClass.java
@@ -1,12 +1,19 @@
 public class MainClass {
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
+        Scanner sc=new Scanner(System.in);
         Circle circle = new Circle();
-        circle.printArea(100);
+        System.out.println("Enter radius of circle");
+        int radius=sc.nextInt();
+        circle.printArea(radius);
 
         Rectangle rectangle = new Rectangle();
-        rectangle.printArea(100, 65);
+        System.out.println("Enter length and breadth of rectangle");
+        int length=sc.nextInt();
+        int breadth=sc.nextInt();
+        rectangle.printArea(length, breadth);
     }
 }
 
