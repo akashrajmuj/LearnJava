e36c93b36bff6c493a31ea411a7bcfb394d9a7b3
AnJiu <orange@MacBook-Pro.local>
2020-10-03 23:38:22 +0800

    exercise

--- chapter2/Example2_9.java
+++ chapter2/Example2_9.java
@@ -1,6 +1,6 @@
 public class Example2_9 {
     public static void main(String[] args) {
-        Weekday x = Weakday.星期日;
+        Weekday x = Weekday.星期日;
         if (x == Weekday.星期日) {
             System.out.println(x);
             System.out.println("今天我休息");
